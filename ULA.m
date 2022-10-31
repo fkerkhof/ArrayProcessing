01232bff6be0c2de194a3f4f5bf8502beea050a0
fkerkhof <franckkerkhof@gmail.com>
2022-10-31 10:46:35 +0100

    Final code

--- ULA.m
+++ ULA.m
@@ -6,6 +6,7 @@ doa_est_plot(X,Delta,M,N);
 load("spcom_50sep.mat");
 doa_est_plot(X,Delta,M,N);
 
+% Function to plot DOA estimate using
 function [] = doa_est_plot(X,Delta,M,N)
     theta = -90:90;
     theta_rad = theta*pi/180;
@@ -26,9 +27,10 @@ function [] = doa_est_plot(X,Delta,M,N)
         spect_est_bartlett(i) = (a'*Rx*a)/(a'*a);
     end
     
-    Pbartlett = 10*log10(abs(spect_est_bartlett));
+    Pbartlett = abs(spect_est_bartlett);
+    Pbartlett_db = 10*log10(abs(spect_est_bartlett));
     figure;
-    plot(theta,Pbartlett);
+    plot(theta,Pbartlett_db);
     hold on;
     
     %% Calculating the spatial power spectrum using MVDR
@@ -38,8 +40,9 @@ function [] = doa_est_plot(X,Delta,M,N)
         spect_est_mvdr(i) = 1/(a'/Rx*a);
     end
     
-    Pmvdr = 10*log10(abs(spect_est_mvdr));
-    plot(theta,Pmvdr);
+    Pmvdr = abs(spect_est_mvdr);
+    Pmvdr_db = 10*log10(abs(spect_est_mvdr));
+    plot(theta,Pmvdr_db);
     
     
     
@@ -58,17 +61,26 @@ function [] = doa_est_plot(X,Delta,M,N)
     spect_est_music = zeros(length(theta_rad),1);
     for i = 1:length(theta_rad)
         a = sv(theta_rad(i),M,Delta);
-        spect_est_music(i) = (a'*a)/(a'*(Un*Un')*a);
+        spect_est_music(i) = 1/(a'*(Un*Un')*a);
     end
     
-    Pmusic = 10*log10(abs(spect_est_music));
-    plot(theta,Pmusic);
+    Pmusic = abs(spect_est_music);
+    Pmusic_db = 10*log10(abs(spect_est_music));
+    plot(theta,Pmusic_db);
     
 
     %% Estimation of DOA using ESPRIT
 
-   [U,~] = eig(Rx);
-   Us = U(:,1:2);
+   [U,Lamda] = eig(Rx);
+   Lamda = diag(Lamda);
+   nsources = 0;
+   for i = 1:length(Rx)
+       if (Lamda(i) > 0.01)
+           nsources = nsources + 1;
+       end
+   end
+
+   Us = U(:,1:nsources);
    Phi = Us(1:end-1,:)\Us(2:end,:);
    Lamda = eig(Phi);
    DoA = -asind(angle(Lamda)/(2*pi*Delta));
@@ -83,7 +95,6 @@ function [] = doa_est_plot(X,Delta,M,N)
 
 end
 
-%% Functions
 %function to calculate the steering vector
 function a = sv(theta, M, Delta)
     a = exp(2j*pi*Delta*(0:M-1)*sin(theta))';
